5eaa1d7b7879a0cc5974adc58d17ea8684acb473
Scratch-Pads <meta.scratchpads@gmail.com>
2021-11-01 21:36:21 +0000

    tutorial display now shows cards location on canvas

--- src/tutorial_stuff/tutorial_display.js
+++ src/tutorial_stuff/tutorial_display.js
@@ -8,6 +8,8 @@ const Tutorial_display = (props) => {
             <p>phase: {props.phase}</p>
             <p>cards to recall: {props.cards_to_recall}</p>
             <p>cards recalled: {props.cards_recalled}</p>
+            {props.panel === 6 && props.phase === 1 ? props.shuffled_deck[props.cards_to_recall] : null}
+            {props.panel === 6 && props.phase === 3 ? props.shuffled_deck[props.cards_recalled] : null}
         </div>
     )
 }
